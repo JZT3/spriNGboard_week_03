2c4e0649237d63730cdf1c576f870df037f50473
jzt3 <jzt3@example.com>
2023-02-12 15:28:26 -0600

    completing the second exercise of week 3

--- disparate/disparate.cc
+++ disparate/disparate.cc
@@ -6,11 +6,9 @@ using namespace std;
 int main ()
 {
   {
-    /////////////////////////////////
-    // CREATE THREE int32_t VALUES //
-    // (big, small, sum)           //
-    /////////////////////////////////
-
+    int32_t big = 100000000;
+    int32_t small = 1;
+    int32_t sum = big + small;
 
     cout << "int32_t:" << endl;
     cout << "  big   = " << big   << endl;
@@ -19,11 +17,9 @@ int main ()
   }
 
   {
-    ///////////////////////////////
-    // CREATE THREE float VALUES //
-    // (big, small, sum)         //
-    ///////////////////////////////
-
+    float big = 100000000;
+    float small = 1;
+    float sum = big+small;
 
     cout << "float:" << endl;
     cout << "  big   = " << fixed << setprecision(1) << big   << endl;
