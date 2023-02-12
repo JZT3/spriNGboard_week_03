a45d31043d84dee34ec4a4c79981d5e8f94cef7f
jzt3 <jzt3@example.com>
2023-02-12 15:13:27 -0600

    completing the first exercise of week 3

--- overflow_underflow/overflow_underflow.cc
+++ overflow_underflow/overflow_underflow.cc
@@ -1,25 +1,21 @@
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
 int main ()
 {
   {
-    ///////////////////////////////////
-    // CREATE SIGNED,UNSIGNED 32-BIT //
-    // INTEGERS NAMED s,u AND        //
-    // INITIALIZE THEM TO ZERO       //
-    ///////////////////////////////////
+    int32_t s = 0;
+    uint32_t u = 0;
 
 
     cout << "32-bit before decrement:" << endl;
     cout << "  signed:   " << s << endl;
     cout << "  unsigned: " << u << endl;
 
-    ///////////////////
-    // DECREMENT s,u //
-    ///////////////////
-
+    s--;
+    u--;
 
     cout << "32-bit after decrement:" << endl;
     cout << "  signed:   " << s << endl;
@@ -27,21 +23,15 @@ int main ()
   }
 
   {
-    ///////////////////////////////////
-    // CREATE SIGNED,UNSIGNED 16-BIT //
-    // INTEGERS NAMED s,u AND        //
-    // INITIALIZE THEM TO MAXIMUMS   //
-    ///////////////////////////////////
+    int16_t s = INT_MAX;
+    uint16_t u = UINT_MAX;
 
-
-    cout << "16-bit before increment:" << endl;
+    cout << "32-bit --> 16-bit conversion:" << endl;
     cout << "  signed:   " << s << endl;
     cout << "  unsigned: " << u << endl;
 
-    ////////////////////////
-    // INCREMENT s,u HERE //
-    ////////////////////////
-
+    s++;
+    u++;
 
     cout << "16-bit after increment:" << endl;
     cout << "  signed:   " << s << endl;
